4fc5323627834ef0a3f0da7973cb61c59f1a7a67
hleebarton97 <hleebarton97@gmail.com>
2019-12-01 14:36:11 -0600

    feature: update report

--- api/routes.js
+++ api/routes.js
@@ -10,9 +10,18 @@ const getFakeDatabase = () => {
 }
 
 const getDatasourceIndexById = (database, id) => {
-  // Get index of object by id
+  // Get index of datasource by id
   for (let i = 0; i < database.datasources.data.length; i++) {
     if (Number(database.datasources.data[i].id) === Number(id)) {
+      return i
+    }
+  }
+}
+
+const getReportIndexById = (database, id) => {
+  // Get index of report by id
+  for (let i = 0; i < database.reports.data.length; i++) {
+    if (Number(database.reports.data[i].report_id) === Number(id)) {
       return i
     }
   }
@@ -23,14 +32,14 @@ const getDatasourceData = body => {
   return {
     data: {
       resultMD: [
-        { colName: 'ID', colType: 'INTEGER', colNum: 1, colAlias: null, colMod: false },
-        { colName: 'Description', colType: 'VARCHAR', colNum: 2, colAlias: null, colMod: false },
-        { colName: 'Cost', colType: 'DECIMAL', colNum: 3, colAlias: null, colMod: false }
+        { colName: 'product_id', colNum: 1, colAlias: null },
+        { colName: 'product_desc', colNum: 2, colAlias: null },
+        { colName: 'product_cost', colNum: 3, colAlias: null }
       ],
       resultSet: [
-        { Description: 'chair', ID: 1, Cost: 50.09 },
-        { Description: 'shirt', ID: 2, Cost: 16.99 },
-        { Description: 'candy', ID: 2, Cost: 5.56 }
+        { product_desc: 'chair', product_id: 1, product_cost: 50.09 },
+        { product_desc: 'shirt', product_id: 2, product_cost: 16.99 },
+        { product_desc: 'candy', product_id: 3, product_cost: 5.56 }
       ]
     }
   }
@@ -54,6 +63,19 @@ module.exports = app => {
         setTimeout(() => res.status(200).send(fakeDatabase.datasources), 1500)
       }
   })
+
+  // GET all reports
+  app.get('/rest/reports',
+    (req, res) => {
+      // Get fake data - reports
+      let fakeDatabase = getFakeDatabase()
+      // Send fake data
+      if (TEST_ERROR) {
+        setTimeout(() => res.status(404).send('Not Found'), 2000)
+      } else {
+        setTimeout(() => res.status(200).send(fakeDatabase.reports), 1500)
+      }
+    })
 
   /// /////////////////
   // P O S T
@@ -95,17 +117,61 @@ module.exports = app => {
       )
   })
 
-    // POST report template
-    app.post('/rest/reports/template',
-    (req, res) => {
-      let datasourceData = getDatasourceData(req.body)
-      // Get fake data - run query
-      if (TEST_ERROR) {
-        setTimeout(() => res.status(404).send('Not Found'), 2000)
-      } else {
-        setTimeout(() => res.status(200).send(datasourceData), 1500)
+  // POST report template
+  app.post('/rest/reports/template',
+  (req, res) => {
+    let datasourceData = getDatasourceData(req.body)
+    // Get fake data - run query
+    if (TEST_ERROR) {
+      setTimeout(() => res.status(404).send('Not Found'), 2000)
+    } else {
+      setTimeout(() => res.status(200).send(datasourceData), 1500)
+    }
+  })
+
+  // POST create a report
+  app.post('/rest/reports',
+  (req, res) => {
+    let fakeDatabase = getFakeDatabase()
+    let id = (fakeDatabase.reports.data.length > 0) ? fakeDatabase.reports.data[fakeDatabase.reports.data.length - 1].report_id + 1 : 1
+    // Create datasource object
+    let reportObject = {
+      report_id: id,
+      datasource_id: req.body.datasource_id,
+      query_string: req.body.query_string,
+      report_title: req.body.report_title,
+      report_desc: req.body.report_desc,
+      resultMD: req.body.resultMD
+    }
+    fakeDatabase.reports.data.push(reportObject)
+    fs.writeFile(
+      path.resolve(__dirname, '../database/data.json'),
+      JSON.stringify(fakeDatabase),
+      (err) => {
+        if (err) {
+          res.status(500).send('Error writing to database')
+        } else {
+          if (TEST_ERROR) {
+            res.status(404).send('Not Found')
+          } else {
+            res.status(200).send(JSON.stringify({data: reportObject}))
+          }
+        }
       }
-    })
+    )
+  })
+
+  // POST run a report
+  app.post('/rest/reports/run',
+  (req, res) => {
+    let datasourceData = getDatasourceData(req.body)
+    // Get fake data - run query
+    if (TEST_ERROR) {
+      setTimeout(() => res.status(404).send('Not Found'), 2000)
+    } else {
+      setTimeout(() => res.status(200).send(datasourceData), 1500)
+    }
+  })
 
   /// /////////////////
   // P U T
@@ -143,6 +209,36 @@ module.exports = app => {
       )
     })
 
+  // Update a report
+  app.put('/rest/reports/:id',
+    (req, res) => {
+      // Get param
+      let id = req.params.id
+      // Get database
+      let fakeDatabase = getFakeDatabase()
+      // Get index of report by id
+      let index = getReportIndexById(fakeDatabase, id)
+      // Update object
+      fakeDatabase.reports.data[index].report_title = req.body.report_title ? req.body.report_title : fakeDatabase.reports.data[index].report_title
+      fakeDatabase.reports.data[index].report_desc = req.body.report_desc ? req.body.report_desc : fakeDatabase.reports.data[index].report_desc
+      // Write to database
+      fs.writeFile(
+        path.resolve(__dirname, '../database/data.json'),
+        JSON.stringify(fakeDatabase),
+        (err) => {
+          if (err) {
+            res.status(500).send('Error writing to database')
+          } else {
+            if (TEST_ERROR) {
+              res.status(404).send('Not Found')
+            } else {
+              res.status(200).send(JSON.stringify({data: fakeDatabase.reports.data[index]}))
+            }
+          }
+        }
+      )
+    })
+
   /// /////////////////
   // D E L E T E
   /// /////////////////
@@ -154,8 +250,6 @@ module.exports = app => {
       let id = req.params.id
       // Get database
       let fakeDatabase = getFakeDatabase()
-      // Get index of object by id
-      let index = getDatasourceIndexById(fakeDatabase, id)
       // Filter array by id
       let filteredData = fakeDatabase.datasources.data.filter(
         (datasource) => {
@@ -163,6 +257,39 @@ module.exports = app => {
       })
       // Set new data set
       fakeDatabase.datasources.data = filteredData
+      // Write to database
+      fs.writeFile(
+        path.resolve(__dirname, '../database/data.json'),
+        JSON.stringify(fakeDatabase),
+        err => {
+          if (err) {
+            res.status(500).send('Error writing to database')
+          } else {
+            if (TEST_ERROR) {
+              res.status(404).send('Not Found')
+            } else {
+              res.status(200).send(JSON.stringify({data: { message: "Datasource deleted" }}))
+            }
+          }
+        }
+      )
+    })
+
+  // Delete a report
+  app.delete('/rest/reports/:id',
+    (req, res) => {
+      // Get param
+      let id = req.params.id
+      // Get database
+      let fakeDatabase = getFakeDatabase()
+      // Filter array by id
+      let filteredData = fakeDatabase.reports.data.filter(
+        (report) => {
+          return Number(report.report_id) !== Number(id)
+        }
+      )
+      // Set new data set
+      fakeDatabase.reports.data = filteredData
       // Write to database
       fs.writeFile(
         path.resolve(__dirname, '../database/data.json'),
